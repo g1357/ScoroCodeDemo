aaea601dd9793494e43f10c0eceeb33fd711c8a7
g1357 <36661604+g1357@users.noreply.github.com>
2018-02-20 20:34:59 +0300

    Commit

---
+++ ScoroTask/ScoroTask/ViewModels/MainViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+using Caliburn.Micro;
+
+using ScoroTask.Helpers;
+
+namespace ScoroTask.ViewModels
+{
+    public class MainViewModel : Screen
+    {
+        public MainViewModel()
+        {
+        }
+    }
+}
---
+++ ScoroTask/ScoroTask/Views/ShellPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using Caliburn.Micro;
+
+using ScoroTask.Services;
+using ScoroTask.ViewModels;
+
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
+
+namespace ScoroTask.Views
+{
+    public sealed partial class ShellPage : IShellView
+    {
+        private ShellViewModel ViewModel => DataContext as ShellViewModel;
+
+        public ShellPage()
+        {
+            InitializeComponent();
+        }
+
+        public INavigationService CreateNavigationService(WinRTContainer container)
+        {
+            return container.RegisterNavigationService(shellFrame);
+        }
+
+        private void OnStateChanged(object sender, VisualStateChangedEventArgs e)
+        {
+            ViewModel.StateChanged(e);
+        }
+    }
+}
