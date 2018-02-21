aba9c651499e9bfbb698ad5b0e367cc64537889f
g1357 <36661604+g1357@users.noreply.github.com>
2018-02-21 18:59:54 +0300

    Soma Additions

--- ScorocodeUWP/Requests/Data/RequestFind.cs
+++ ScorocodeUWP/Requests/Data/RequestFind.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using ScorocodeUWP.ScorocodeObjects;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,49 @@ namespace ScorocodeUWP.Requests.Data
 {
     public class RequestFind
     {
+        [JsonProperty("app")]
+        private string app;
+        [JsonProperty("cli")]
+        private string cli;
+        [JsonProperty("acc")]
+        private string acc;
+        [JsonProperty("sess")]
+        private string sess;
+        [JsonProperty("coll")]
+        private string coll;
+        [JsonProperty("query")]
+        private Dictionary<string, object> query;
+        [JsonProperty("sort")]
+        private Dictionary<string, int> sort;
+        [JsonProperty("fields")]
+        private List<string> fields;
+        [JsonProperty("limit")]
+        private int limit;
+        [JsonProperty("skip")]
+        private int skip;
+
+        public RequestFind(ScorocodeCoreInfo stateHolder, string coll,
+                Query query, SortInfo sort, List<String> fields,
+                int limit, int skip)
+        {
+            this.app = stateHolder.getApplicationId();
+            this.cli = stateHolder.getClientKey();
+            this.acc = stateHolder.getMasterKey();
+            this.sess = stateHolder.getSessionId();
+            this.coll = coll;
+
+            if (query != null)
+            {
+                this.query = query.getQueryInfo(); //.getInfo();
+            }
+            if (sort != null)
+            {
+                this.sort = sort.Info; //.getInfo();
+            }
+            this.fields = fields;
+            this.limit = limit;
+            this.skip = skip;
+        }
+
     }
 }
